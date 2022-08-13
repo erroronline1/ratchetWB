dc954bdcf0754dda5cfc23a494e6fce8eb2037cf
erroronline1 <dev@erroronline.one>
2022-08-14 01:04:26 +0200

    renamed variables for cleaner gui-appearance

--- freecad/ratchetWB/teeth.py
+++ freecad/ratchetWB/teeth.py
@@ -2,24 +2,24 @@ import math as Math
 
 class DirectedTeeth():
     def __init__(self, properties):
-        self.radius = properties['radius']
-        self.teeth = properties['teeth']
-        self.toothheight = properties['toothheight']
-        self.inset = properties['inset']
-        self.pad = properties['pad']
+        self.Radius = properties['Radius']
+        self.Teeth = properties['Teeth']
+        self.Toothheight = properties['Toothheight']
+        self.Inset = properties['Inset']
+        self.Pad = properties['Pad']
         self._calc_directed()
 
     def _calc_directed(self):
-        tooth = Math.pi*2 / self.teeth
-        floor=self.radius - self.toothheight
-        if self.inset:
-            middle=floor + self.toothheight/4
+        tooth = Math.pi*2 / self.Teeth
+        floor=self.Radius - self.Toothheight
+        if self.Inset:
+            middle=floor + self.Toothheight/4
         else:
-            middle=floor + self.toothheight/2
+            middle=floor + self.Toothheight/2
         segments=[];
         segments.append({'type': 'p', 'x': Math.sin(0) * floor, 'y': Math.cos(0) * floor, 'z': 0}) # start point line
-        for i in range(self.teeth):
-            segments.append({'type': 'p', 'x': Math.sin(tooth * i) * self.radius, 'y': Math.cos(tooth * i) * self.radius, 'z': 0}) # end point line, start point arc
+        for i in range(self.Teeth):
+            segments.append({'type': 'p', 'x': Math.sin(tooth * i) * self.Radius, 'y': Math.cos(tooth * i) * self.Radius, 'z': 0}) # end point line, start point arc
             segments.append({'type': 'r', 'x': Math.sin(tooth * (i + .5)) * middle, 'y': Math.cos(tooth * (i + .5)) * middle, 'z': 0}) # arc rim point
             segments.append({'type': 'p', 'x': Math.sin(tooth * (i + 1)) * floor, 'y': Math.cos(tooth * (i + 1)) * floor, 'z': 0}) # end point arc
         self.segments = segments
@@ -29,19 +29,19 @@ class DirectedTeeth():
 
 class DoubleTeeth():
     def __init__(self, properties):
-        self.radius = properties['radius']
-        self.teeth = properties['teeth']
-        self.toothheight = properties['toothheight']
-        self.pad = properties['pad']
+        self.Radius = properties['Radius']
+        self.Teeth = properties['Teeth']
+        self.Toothheight = properties['Toothheight']
+        self.Pad = properties['Pad']
         self._calc_directed()
 
     def _calc_directed(self):
-        tooth = Math.pi*2 / self.teeth
-        floor=self.radius - self.toothheight
+        tooth = Math.pi*2 / self.Teeth
+        floor=self.Radius - self.Toothheight
         segments=[];
-        for i in range(self.teeth):
+        for i in range(self.Teeth):
             segments.append({'type': 'p', 'x': Math.sin(tooth * i) * floor, 'y': Math.cos(tooth * i) * floor, 'z': 0}) # end point
-            segments.append({'type': 'p', 'x': Math.sin(tooth * (i +.5)) * self.radius, 'y': Math.cos(tooth * (i +.5)) * self.radius, 'z': 0}) # tip
+            segments.append({'type': 'p', 'x': Math.sin(tooth * (i +.5)) * self.Radius, 'y': Math.cos(tooth * (i +.5)) * self.Radius, 'z': 0}) # tip
         self.segments = segments
 
     def _update(self):
--- freecad/ratchetWB/tools.py
+++ freecad/ratchetWB/tools.py
@@ -62,35 +62,35 @@ class Directed(BaseRatchet):
     def __init__(self, obj):
         super(Directed, self).__init__(obj)
         properties={
-            'radius': 25,
-            'teeth': 15,
-            'toothheight': 5,
-            'inset': False,
-            'pad': 5
+            'Radius': 25,
+            'Teeth': 15,
+            'Toothheight': 5,
+            'Inset': False,
+            'Pad': 5
         }
         self.ratchet = teeth.DirectedTeeth(properties)
 
-        obj.addProperty("App::PropertyLength", "radius", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyRadius")[0])
-        obj.addProperty("App::PropertyInteger", "teeth", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyTeeth")[0])
-        obj.addProperty("App::PropertyLength", "toothheight", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyToothheight")[0])
-        obj.addProperty("App::PropertyBool", "inset", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyInset")[0])
-        obj.addProperty("App::PropertyLength", "pad", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyPad")[0])
+        obj.addProperty("App::PropertyLength", "Radius", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyRadius")[0])
+        obj.addProperty("App::PropertyInteger", "Teeth", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyTeeth")[0])
+        obj.addProperty("App::PropertyLength", "Toothheight", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyToothheight")[0])
+        obj.addProperty("App::PropertyBool", "Inset", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyInset")[0])
+        obj.addProperty("App::PropertyLength", "Pad", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyPad")[0])
         obj.addProperty("App::PropertyPythonObject", "ratchet", LANG.chunk("PropertyTitle")[0], "ratchet object")
 
-        obj.radius = f"{properties['radius']}. mm"
-        obj.teeth = properties['teeth']
-        obj.toothheight = f"{properties['toothheight']}. mm"
-        obj.inset = properties['inset']
-        obj.pad = f"{properties['pad']}. mm"
+        obj.Radius = f"{properties['Radius']}. mm"
+        obj.Teeth = properties['Teeth']
+        obj.Toothheight = f"{properties['Toothheight']}. mm"
+        obj.Inset = properties['Inset']
+        obj.Pad = f"{properties['Pad']}. mm"
         obj.ratchet = self.ratchet
         obj.Proxy = self
 
     def _generate_ratchet(self, fp):
-        fp.ratchet.radius = fp.radius
-        fp.ratchet.teeth = fp.teeth
-        fp.ratchet.toothheight = fp.toothheight
-        fp.ratchet.inset = fp.inset
-        fp.ratchet.pad = fp.pad
+        fp.ratchet.Radius = fp.Radius
+        fp.ratchet.Teeth = fp.Teeth
+        fp.ratchet.Toothheight = fp.Toothheight
+        fp.ratchet.Inset = fp.Inset
+        fp.ratchet.Pad = fp.Pad
         fp.ratchet._update()
                 
         vector=fp.ratchet.segments
@@ -114,7 +114,7 @@ class Directed(BaseRatchet):
                 nextv = v+2
         wire=Part.Wire(draft)
         face=Part.Face(wire)
-        return face.extrude(FreeCAD.Vector(0, 0, fp.ratchet.pad))
+        return face.extrude(FreeCAD.Vector(0, 0, fp.ratchet.Pad))
 
     def __getstate__(self):
         return None
@@ -127,31 +127,31 @@ class Double(BaseRatchet):
     def __init__(self, obj):
         super(Double, self).__init__(obj)
         properties={
-            'radius': 25,
-            'teeth': 15,
-            'toothheight': 5,
-            'pad': 5
+            'Radius': 25,
+            'Teeth': 15,
+            'Toothheight': 5,
+            'Pad': 5
         }
         self.ratchet = teeth.DoubleTeeth(properties)
 
-        obj.addProperty("App::PropertyLength", "radius", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyRadius")[0])
-        obj.addProperty("App::PropertyInteger", "teeth", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyTeeth")[0])
-        obj.addProperty("App::PropertyLength", "toothheight", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyToothheight")[0])
-        obj.addProperty("App::PropertyLength", "pad", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyPad")[0])
+        obj.addProperty("App::PropertyLength", "Radius", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyRadius")[0])
+        obj.addProperty("App::PropertyInteger", "Teeth", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyTeeth")[0])
+        obj.addProperty("App::PropertyLength", "Toothheight", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyToothheight")[0])
+        obj.addProperty("App::PropertyLength", "Pad", LANG.chunk("PropertyTitle")[0], LANG.chunk("PropertyPad")[0])
         obj.addProperty("App::PropertyPythonObject", "ratchet", "Parameter", "ratchet object")
 
-        obj.radius = f"{properties['radius']}. mm"
-        obj.teeth = properties['teeth']
-        obj.toothheight = f"{properties['toothheight']}. mm"
-        obj.pad = f"{properties['pad']}. mm"
+        obj.Radius = f"{properties['Radius']}. mm"
+        obj.Teeth = properties['Teeth']
+        obj.Toothheight = f"{properties['Toothheight']}. mm"
+        obj.Pad = f"{properties['Pad']}. mm"
         obj.ratchet = self.ratchet
         obj.Proxy = self
 
     def _generate_ratchet(self, fp):
-        fp.ratchet.radius = fp.radius
-        fp.ratchet.teeth = fp.teeth
-        fp.ratchet.toothheight = fp.toothheight
-        fp.ratchet.pad = fp.pad
+        fp.ratchet.Radius = fp.Radius
+        fp.ratchet.Teeth = fp.Teeth
+        fp.ratchet.Toothheight = fp.Toothheight
+        fp.ratchet.Pad = fp.Pad
         fp.ratchet._update()
                 
         vector=fp.ratchet.segments
@@ -166,7 +166,7 @@ class Double(BaseRatchet):
                                                     FreeCAD.Vector(vector[0]['x'], vector[0]['y'], vector[0]['z'])).toShape())
         wire=Part.Wire(draft)
         face=Part.Face(wire)
-        return face.extrude(FreeCAD.Vector(0, 0, fp.ratchet.pad))
+        return face.extrude(FreeCAD.Vector(0, 0, fp.ratchet.Pad))
 
     def __getstate__(self):
         return None
