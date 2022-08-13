544ec15d927133318f22cdb23c1748d6a187ef35
erroronline1 <dev@erroronline.one>
2022-08-14 00:45:22 +0200

    fulfilled base command

--- freecad/ratchetWB/commands.py
+++ freecad/ratchetWB/commands.py
@@ -1,53 +1,87 @@
-import FreeCADGui
+import FreeCAD, FreeCADGui
 import os
 from . import ICONPATH
 from . import tools
 
 class BaseCommand():
-	fnParams = None
-	FreeCADGui.doCommandGui("import freecad.ratchetWB.commands")
+    # taken from https://github.com/looooo/freecad.gears
+    def __init__(self):
+        pass
 
-	def __init__(self):
-		pass
+    def IsActive(self):
+        if FreeCAD.ActiveDocument is None:
+            return False
+        return True
 
-	def GetResources(self):
-		return {'Pixmap': self.pixmap,
-				'MenuText': self.menuText,
-				'ToolTip': self.toolTip}
+    def GetResources(self):
+        return {'Pixmap': self.pixmap,
+                'MenuText': self.menuText,
+                'ToolTip': self.toolTip}
 
-	def Activated(self):
-		FreeCADGui.doCommandGui(f"freecad.ratchetWB.commands.{self.__class__.__name__}.do('{self.fnParams}')")
+    def Activated(self):
+        doc = FreeCAD.ActiveDocument
+        FreeCADGui.doCommandGui("import freecad.ratchetWB.commands")
+        doc.openTransaction(self.toolTip)
+        FreeCADGui.doCommandGui(f'freecad.ratchetWB.commands.{self.__class__.__name__}.create()')
+        doc.commitTransaction()
+        doc.recompute()
+        FreeCADGui.SendMsgToActiveView("ViewFit")
 
-	@classmethod
-	def do(self, fnParams=None):
-		self.function(fnParams)
+    @classmethod
+    def create(cls):
+        if FreeCAD.GuiUp:
+            body = FreeCADGui.ActiveDocument.ActiveView.getActiveObject("pdbody")
+            part = FreeCADGui.ActiveDocument.ActiveView.getActiveObject("part")
+
+            if body:
+                obj = FreeCAD.ActiveDocument.addObject("PartDesign::FeaturePython", cls.name)
+            else:
+                obj = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", cls.name)
+            ViewRatchet(obj.ViewObject, cls.pixmap)
+            cls.function(obj)
+
+            if body:
+                body.addObject(obj)
+            elif part:
+                part.Group += [obj]
+        else:
+            obj = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", cls.name)
+            cls.function(obj)
+        return obj
 
 ################################################################################
 
-class Create_ExternalDirected(BaseCommand):
-	name = tools.LANG.chunk("ExternalDirectedName")[0]
-	function = tools.ExternalDirected
-	pixmap = os.path.join(ICONPATH, "icon.svg")
-	menuText = tools.LANG.chunk("ExternalDirectedMenuText")[0]
-	toolTip = tools.LANG.chunk("ExternalDirectedToolTip")[0]
+class ViewRatchet(object):
+    # taken from https://github.com/looooo/freecad.gears
+    def __init__(self, obj, icon):
+        ''' Set this object to the proxy object of the actual view provider '''
+        obj.Proxy = self
+        self.icon = icon
 
-class Create_InternalDirected(BaseCommand):
-	name = tools.LANG.chunk("InternalDirectedName")[0]
-	function = tools.InternalDirected
-	pixmap = os.path.join(ICONPATH, "InternalDirected.svg")
-	menuText = tools.LANG.chunk("InternalDirectedMenuText")[0]
-	toolTip = tools.LANG.chunk("InternalDirectedToolTip")[0]
+    def attach(self, vobj):
+        self.vobj = vobj
 
-class Create_ExternalDouble(BaseCommand):
-	name = tools.LANG.chunk("ExternalDoubleName")[0]
-	function = tools.ExternalDouble
-	pixmap = os.path.join(ICONPATH, "ExternalDouble.svg")
-	menuText = tools.LANG.chunk("ExternalDoubleMenuText")[0]
-	toolTip = tools.LANG.chunk("ExternalDoubleToolTip")[0]
+    def getIcon(self):
+        return self.icon
 
-class Create_InternalDouble(BaseCommand):
-	name = tools.LANG.chunk("InternalDoubleName")[0]
-	function = tools.InternalDouble
-	pixmap = os.path.join(ICONPATH, "InternalDouble.svg")
-	menuText = tools.LANG.chunk("InternalDoubleMenuText")[0]
-	toolTip = tools.LANG.chunk("InternalDoubleToolTip")[0]
+    def __getstate__(self):
+        return None
+
+    def __setstate__(self, state):
+        return None
+
+################################################################################
+
+class Create_Directed(BaseCommand):
+    name = tools.LANG.chunk("DirectedName")[0]
+    function = tools.Directed
+    pixmap = os.path.join(ICONPATH, "icon.svg")
+    menuText = tools.LANG.chunk("DirectedMenuText")[0]
+    toolTip = tools.LANG.chunk("DirectedToolTip")[0]
+
+class Create_Double(BaseCommand):
+    name = tools.LANG.chunk("DoubleName")[0]
+    function = tools.Double
+    pixmap = os.path.join(ICONPATH, "Double.svg")
+    menuText = tools.LANG.chunk("DoubleMenuText")[0]
+    toolTip = tools.LANG.chunk("DoubleToolTip")[0]
